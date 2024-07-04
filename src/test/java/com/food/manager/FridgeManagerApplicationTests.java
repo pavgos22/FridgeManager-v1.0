36a611f96b982813b12caf5a618e09d2041f5c81
Pawel <pawgosciak@gmail.com>
2024-07-04 18:16:38 +0200

    initial commit

---
+++ src/test/java/com/food/manager/FridgeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
